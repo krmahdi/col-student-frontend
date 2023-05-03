1745099a766314ec3cd8b0946d13ca1b64253944
karouimahdi <98275441+karouimahdi@users.noreply.github.com>
2023-05-03 21:24:50 +0100

    leafet integration

--- src/app/map-component/map-component.component.ts
+++ src/app/map-component/map-component.component.ts
@@ -1,26 +1,25 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
 import { AnnonceService } from '../services/annonce.service';
 import { Annonce } from '../interfaces/annonce.interface';
-import { HttpClient, HttpParams} from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
 @Component({
   selector: 'app-root',
   templateUrl: './map-component.component.html',
-  styleUrls: ['./map-component.component.css']
+  styleUrls: ['./map-component.component.css'],
 })
-export class MapComponentComponent implements OnInit, AfterViewInit {
- private map: L.Map;
- query: string = '';
-   filterForm:any={}
+export class MapComponentComponent implements OnInit {
+  map: L.Map;
+
   homeCoords = {
-    lat: 33.8439408,
-    lon: 9.400138
+    lat: 36.8252687,
+    lon: 10.3106223,
   };
-  
   annonces: Annonce[] = [];
-    popupText = 'Some popup text';
+
+  popupText = 'Some popup text';
 
   markerIcon = {
     icon: L.icon({
@@ -28,80 +27,48 @@ export class MapComponentComponent implements OnInit, AfterViewInit {
       iconAnchor: [10, 41],
       popupAnchor: [2, -40],
       // specify the path here
-      iconUrl:
-        'https://unpkg.com/leaflet@1.4.0/dist/images/marker-icon.png',
+      iconUrl: 'https://unpkg.com/leaflet@1.4.0/dist/images/marker-icon.png',
       shadowUrl:
-        'https://unpkg.com/leaflet@1.4.0/dist/images/marker-shadow.png'
-    })
+        'https://unpkg.com/leaflet@1.4.0/dist/images/marker-shadow.png',
+    }),
   };
 
   options = {
     layers: [
       L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 100,
-        attribution: ''
-      })
+        maxZoom: 18,
+        attribution: '',
+      }),
     ],
-    zoom: 5,
-    center: L.latLng(this.homeCoords.lat, this.homeCoords.lon)
+    zoom: 10,
+    center: L.latLng(this.homeCoords.lat, this.homeCoords.lon),
   };
-
-  constructor(private annonceService: AnnonceService,private http:HttpClient) {}
+  constructor(
+    private annonceService: AnnonceService,
+    private http: HttpClient
+  ) {}
 
   ngOnInit() {
-    this.getAnnonces();
-   
+    const mapContainer = L.DomUtil.get('map') || '';
+    const mapOptions = this.options;
+    this.map = L.map(mapContainer, mapOptions);
 
-  }
-
-  
-  
-
-  ngAfterViewInit() {
-    const map = L.map('map', this.options);
-    this.map = map;
-    L.control.scale().addTo(this.map);
-    
-    // Move the initMarkers() function call inside the callback function
-    this.map.on('load', () => {
-      this.initMarkers();
-    });
-  }
-  
-  getAnnonces() {
-    this.annonceService.getAnnonces().subscribe((data) => {
-      this.annonces = data;
-    });
+    // Do stuff with map
+    this.initMarkers();
   }
 
   initMarkers() {
-    this.annonces.map((annonce) => {
-      const popupInfo = `<b style="color: red; background-color: white">${annonce.loyer}</b>`;
-      return L.marker([annonce.altitude, annonce.longitude], this.markerIcon)
-        .addTo(this.map)
-        .bindPopup(popupInfo);
+    this.annonceService.getAnnonces().subscribe((data) => {
+      data.map((annonce) => {
+        const popupInfo = `<b style="color: red; background-color: white">${annonce.description}</b>`;
+        return L.marker([annonce.altitude, annonce.longitude], this.markerIcon)
+          .addTo(this.map)
+          .bindPopup(popupInfo);
+      });
     });
-  }
-  searchQuery: string = '';
 
-  search() {
-    // do something with the search query
-    const city = this.searchQuery;
-    console.log('searching for', city);
-    const url = `https://nominatim.openstreetmap.org/search?q=${city}&format=json`;
-    this.http.get(url).subscribe((data: any) => {
-      if (data.length > 0) {
-        const lat = parseFloat(data[0].lat);
-        const lon = parseFloat(data[0].lon);
-        console.log(`Latitude: ${lat}, Longitude: ${lon}`);
-        // You can use these latitude and longitude values to display the city on the map
-        const zoomLevel = 10; // adjust zoom level to your liking
-        this.map.setView([lat, lon], zoomLevel);
-      } else {
-        console.log('City not found');
-      }
-    });
+  
   }
-  
-  
+
+ 
 }
