bfe96e5f6a26873dc11b8f070adceb8f0783a029
karouimahdi <98275441+karouimahdi@users.noreply.github.com>
2023-05-07 21:14:06 +0100

    fixing some details

--- src/app/components/annoncdetail/annoncdetail.component.ts
+++ src/app/components/annoncdetail/annoncdetail.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import * as L from 'leaflet';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Annonce } from 'src/app/interfaces/annonce.interface';
 import { AnnonceService } from 'src/app/services/annonce.service';
@@ -19,35 +18,6 @@ import { NgxStarRatingComponent } from 'ngx-star-rating/public-api';
   styleUrls: ['./annoncdetail.component.css'],
 })
 export class AnnoncdetailComponent implements OnInit {
-
-  map: L.Map;
-  homeCoords = {
-    lat: 36.8252687,
-    lon: 10.3106223,
-  };
-  popupText = 'Some popup text';
-  markerIcon = {
-    icon: L.icon({
-      iconSize: [25, 41],
-      iconAnchor: [10, 41],
-      popupAnchor: [2, -40],
-      // specify the path here
-      iconUrl: 'https://unpkg.com/leaflet@1.4.0/dist/images/marker-icon.png',
-      shadowUrl:
-        'https://unpkg.com/leaflet@1.4.0/dist/images/marker-shadow.png',
-    }),
-  };
-  options = {
-    layers: [
-      L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 18,
-        attribution: '',
-      }),
-    ],
-    zoom: 10,
-    center: L.latLng(this.homeCoords.lat, this.homeCoords.lon),
-  };
-
   response: Annonce;
   faSmokingBan = faSmokingBan;
   faSmoking = faSmoking;
@@ -66,12 +36,6 @@ export class AnnoncdetailComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    const mapContainer = L.DomUtil.get('map') || '';
-    const mapOptions = this.options;
-    this.map = L.map(mapContainer, mapOptions);
-
-    // Do stuff with map
-    this.initMarkers();
     // console.log({ response: this.response });
     this.activatedRoute.paramMap.subscribe((params: Params) => {
       console.log(+params['get']('id')!);
@@ -83,13 +47,4 @@ export class AnnoncdetailComponent implements OnInit {
         });
     });
   }
-  initMarkers() {
-  this.popupText=`<div>${this.response.description}</div>`
- 
-        return L.marker([this.response.altitude, this.response.longitude], this.markerIcon)
-          .addTo(this.map)
-          .bindPopup(this.popupText);
-      }
-   
-  }
-
+}
